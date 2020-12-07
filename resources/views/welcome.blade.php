8ad2aad82e7d14a559beb3596bdd223799c53157
dbouidaine <hd_bouidaine@esi.dz>
2020-12-08 00:49:59 +0100

    Some Animations

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,6 +22,10 @@ Coded by www.creative-tim.com
   <title>
     Argon Design System by Creative Tim
   </title>
+
+  <link rel="stylesheet" href="{{ asset('virb') }}/css/styles-merged.css">
+  <link rel="stylesheet" href="{{ asset('virb') }}/css/style.min.css">
+  <link rel="stylesheet" href="{{ asset('virb') }}/css/custom.css">
   <!--     Fonts and icons     -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
   <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
@@ -36,6 +40,7 @@ Coded by www.creative-tim.com
 </head>
 
 <body class="landing-page">
+  <div class="probootstrap-loader"></div>
   <!-- Navbar -->
   <nav id="navbar-main" class="navbar navbar-main navbar-expand-lg navbar-transparent navbar-light py-2">
     <div class="container">
@@ -169,11 +174,11 @@ Coded by www.creative-tim.com
         <span class="span-50"></span>
         <span class="span-100"></span>
       </div>
-      <div class="page-header">
+      <div class="page-header" style="border-bottom:none;">
         <div class="container shape-container d-flex align-items-center py-lg">
           <div class="col px-0">
             <div class="row align-items-center justify-content-center">
-              <div class="col-lg-6 text-center">
+              <div class="col-lg-6 text-center probootstrap-animate" data-animate-effect="fadeIn">
                 <h1 class="text-white display-1">People stories</h1>
                 <h2 class="display-4 font-weight-normal text-white">The time is right now!</h2>
                 <div class="btn-wrapper mt-4">
@@ -188,7 +193,7 @@ Coded by www.creative-tim.com
         </div>
       </div>
     </div>
-    <div class="separator separator-top separator-skew zindex-100" style="position: relative; height:140px;">
+    <div class="separator separator-buttom separator-skew zindex-100 mt--2" style="position: relative; height:140px;">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 283.5 27.8" preserveAspectRatio="none">
             <path class="fill-success" d="M283.5,9.7c0,0-7.3,4.3-14,4.6c-6.8,0.3-12.6,0-20.9-1.5c-11.3-2-33.1-10.1-44.7-5.7	s-12.1,4.6-18,7.4c-6.6,3.2-20,9.6-36.6,9.3C131.6,23.5,99.5,7.2,86.3,8c-1.4,0.1-6.6,0.8-10.5,2c-3.8,1.2-9.4,3.8-17,4.7	c-3.2,0.4-8.3,1.1-14.2,0.9c-1.5-0.1-6.3-0.4-12-1.6c-5.7-1.2-11-3.1-15.8-3.7C6.5,9.2,0,10.8,0,10.8V0h283.5V9.7z M260.8,11.3	c-0.7-1-2-0.4-4.3-0.4c-2.3,0-6.1-1.2-5.8-1.1c0.3,0.1,3.1,1.5,6,1.9C259.7,12.2,261.4,12.3,260.8,11.3z M242.4,8.6	c0,0-2.4-0.2-5.6-0.9c-3.2-0.8-10.3-2.8-15.1-3.5c-8.2-1.1-15.8,0-15.1,0.1c0.8,0.1,9.6-0.6,17.6,1.1c3.3,0.7,9.3,2.2,12.4,2.7	C239.9,8.7,242.4,8.6,242.4,8.6z M185.2,8.5c1.7-0.7-13.3,4.7-18.5,6.1c-2.1,0.6-6.2,1.6-10,2c-3.9,0.4-8.9,0.4-8.8,0.5	c0,0.2,5.8,0.8,11.2,0c5.4-0.8,5.2-1.1,7.6-1.6C170.5,14.7,183.5,9.2,185.2,8.5z M199.1,6.9c0.2,0-0.8-0.4-4.8,1.1	c-4,1.5-6.7,3.5-6.9,3.7c-0.2,0.1,3.5-1.8,6.6-3C197,7.5,199,6.9,199.1,6.9z M283,6c-0.1,0.1-1.9,1.1-4.8,2.5s-6.9,2.8-6.7,2.7	c0.2,0,3.5-0.6,7.4-2.5C282.8,6.8,283.1,5.9,283,6z M31.3,11.6c0.1-0.2-1.9-0.2-4.5-1.2s-5.4-1.6-7.8-2C15,7.6,7.3,8.5,7.7,8.6	C8,8.7,15.9,8.3,20.2,9.3c2.2,0.5,2.4,0.5,5.7,1.6S31.2,11.9,31.3,11.6z M73,9.2c0.4-0.1,3.5-1.6,8.4-2.6c4.9-1.1,8.9-0.5,8.9-0.8	c0-0.3-1-0.9-6.2-0.3S72.6,9.3,73,9.2z M71.6,6.7C71.8,6.8,75,5.4,77.3,5c2.3-0.3,1.9-0.5,1.9-0.6c0-0.1-1.1-0.2-2.7,0.2	C74.8,5.1,71.4,6.6,71.6,6.7z M93.6,4.4c0.1,0.2,3.5,0.8,5.6,1.8c2.1,1,1.8,0.6,1.9,0.5c0.1-0.1-0.8-0.8-2.4-1.3	C97.1,4.8,93.5,4.2,93.6,4.4z M65.4,11.1c-0.1,0.3,0.3,0.5,1.9-0.2s2.6-1.3,2.2-1.2s-0.9,0.4-2.5,0.8C65.3,10.9,65.5,10.8,65.4,11.1	z M34.5,12.4c-0.2,0,2.1,0.8,3.3,0.9c1.2,0.1,2,0.1,2-0.2c0-0.3-0.1-0.5-1.6-0.4C36.6,12.8,34.7,12.4,34.5,12.4z M152.2,21.1	c-0.1,0.1-2.4-0.3-7.5-0.3c-5,0-13.6-2.4-17.2-3.5c-3.6-1.1,10,3.9,16.5,4.1C150.5,21.6,152.3,21,152.2,21.1z"></path>
             <path class="fill-success" d="M269.6,18c-0.1-0.1-4.6,0.3-7.2,0c-7.3-0.7-17-3.2-16.6-2.9c0.4,0.3,13.7,3.1,17,3.3	C267.7,18.8,269.7,18,269.6,18z"></path>
@@ -205,21 +210,21 @@ Coded by www.creative-tim.com
       <div class="container">
         <div class="row align-items-center">
           <div class="col-lg-6">
-            <div class="info info-horizontal info-hover-primary">
+            <div class="info info-horizontal info-hover-primary probootstrap-animate" data-animate-effect="fadeInLeft">
               <div class="description pl-4">
                 <h5 class="title">For Developers</h5>
                 <p>The time is now for it to be okay to be great. People in this world shun people for being great. For being a bright color. For standing out. But the time is now.</p>
                 <a href="#" class="text-info">Learn more</a>
               </div>
             </div>
-            <div class="info info-horizontal info-hover-primary mt-5">
+            <div class="info info-horizontal info-hover-primary mt-5 probootstrap-animate" data-animate-effect="fadeInLeft">
               <div class="description pl-4">
                 <h5 class="title">For Designers</h5>
                 <p>There’s nothing I really wanted to do in life that I wasn’t able to get good at. That’s my skill. I’m not really specifically talented at anything except for the ability to learn.</p>
                 <a href="#" class="text-info">Learn more</a>
               </div>
             </div>
-            <div class="info info-horizontal info-hover-primary mt-5">
+            <div class="info info-horizontal info-hover-primary mt-5 probootstrap-animate" data-animate-effect="fadeInLeft">
               <div class="description pl-4">
                 <h5 class="title">For Beginners</h5>
                 <p>That’s what I do. That’s what I’m here for. Don’t be afraid to be wrong because you can’t learn anything from a compliment. If everything I did failed - which it doesn't.</p>
@@ -227,7 +232,7 @@ Coded by www.creative-tim.com
               </div>
             </div>
           </div>
-          <div class="col-lg-6 col-10 mx-md-auto">
+          <div class="col-lg-6 col-10 mx-md-auto probootstrap-animate" data-animate-effect="fadeInRight">
             <img class="ml-lg-5" src="{{ asset('argon') }}/img/ill/ill.png" width="100%">
           </div>
         </div>
@@ -246,7 +251,7 @@ Coded by www.creative-tim.com
             <path class="fill-info" d="M15.6,13.2c0-0.1,4.3,0,6.7,0.5c2.4,0.5,5,1.9,5,2c0,0.1-2.7-0.8-5.1-1.4	C19.9,13.7,15.7,13.3,15.6,13.2z"></path>
         </svg>
     </div>
-    <div class="section features-1 bg-info">
+    <div class="section features-1 bg-info mt--2">
       <div class="container">
         <div class="row">
           <div class="col-md-8 mx-auto text-center">
@@ -362,6 +367,9 @@ Coded by www.creative-tim.com
   <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
   <script src="../assets/js/argon-design-system.min.js?v=1.2.2" type="text/javascript"></script>
   <script src="https://cdn.trackjs.com/agent/v3/latest/t.js"></script>
+  <script src="{{ asset('virb') }}/js/scripts.min.js"></script>
+  <script src="{{ asset('virb') }}/js/main.min.js"></script>
+  <script src="{{ asset('virb') }}/js/custom.js"></script>
   <script>
     window.TrackJS &&
       TrackJS.install({
